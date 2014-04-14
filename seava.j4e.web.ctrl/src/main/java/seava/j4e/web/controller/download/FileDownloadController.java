11e214f437d8777b24729d5aa60642fc65523be4
Attila Mathe <attila.mathe@dnet-ebusiness-suite.com>
2014-04-14 16:58:33 +0300

    Check if file exists in download controller.

--- seava.j4e.web.ctrl/src/main/java/seava/j4e/web/controller/download/FileDownloadController.java
+++ seava.j4e.web.ctrl/src/main/java/seava/j4e/web/controller/download/FileDownloadController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import seava.j4e.api.Constants;
+import seava.j4e.api.exceptions.BusinessException;
+import seava.j4e.api.exceptions.ErrorCode;
 import seava.j4e.api.session.Session;
 import seava.j4e.web.controller.AbstractBaseController;
 
@@ -29,41 +31,51 @@ public class FileDownloadController extends AbstractBaseController {
 	public String download(@PathVariable String fileName,
 			@PathVariable String dataFormat, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		try {
+			if (logger.isInfoEnabled()) {
+				logger.info("Processing request: download export file ",
+						new Object[] { fileName });
+			}
 
-		if (logger.isInfoEnabled()) {
-			logger.info("Processing request: download export file ",
-					new Object[] { fileName });
+			this.prepareRequest(request, response);
+
+			String path = Session.user.get().getWorkspace().getTempPath();
+
+			File file = new File(path + "/" + fileName + "." + dataFormat);
+			// send file
+
+			if (!file.exists()) {
+				throw new BusinessException(ErrorCode.G_FILE_NOT_FOUND,
+						"File not found");
+			}
+
+			if (dataFormat.equals(Constants.DATA_FORMAT_CSV)) {
+				response.setContentType("application/vnd.ms-excel; charset=UTF-8");
+			}
+			if (dataFormat.equals(Constants.DATA_FORMAT_JSON)) {
+				response.setContentType("text/plain; charset=UTF-8");
+			}
+			if (dataFormat.equals(Constants.DATA_FORMAT_HTML)) {
+				response.setContentType("text/html; charset=UTF-8");
+			}
+			if (dataFormat.equals(Constants.DATA_FORMAT_XML)) {
+				response.setContentType("text/xml; charset=UTF-8");
+			}
+			response.setHeader("Content-Description", "File Transfer");
+			response.setHeader("Content-Disposition", "inline; filename=\""
+					+ fileName + "\";");
+
+			this.sendFile(file, response.getOutputStream());
+			// return null;
+
+			// delete it
+			Files.delete(file.toPath());
+
+			return null;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			this.finishRequest();
 		}
-
-		this.prepareRequest(request, response);
-
-		String path = Session.user.get().getWorkspace().getTempPath();
-
-		File file = new File(path + "/" + fileName + "." + dataFormat);
-		// send file
-
-		if (dataFormat.equals(Constants.DATA_FORMAT_CSV)) {
-			response.setContentType("application/vnd.ms-excel; charset=UTF-8");
-		}
-		if (dataFormat.equals(Constants.DATA_FORMAT_JSON)) {
-			response.setContentType("text/plain; charset=UTF-8");
-		}
-		if (dataFormat.equals(Constants.DATA_FORMAT_HTML)) {
-			response.setContentType("text/html; charset=UTF-8");
-		}
-		if (dataFormat.equals(Constants.DATA_FORMAT_XML)) {
-			response.setContentType("text/xml; charset=UTF-8");
-		}
-		response.setHeader("Content-Description", "File Transfer");
-		response.setHeader("Content-Disposition", "inline; filename=\""
-				+ fileName + "\";");
-
-		this.sendFile(file, response.getOutputStream());
-		// return null;
-
-		// delete it
-		Files.delete(file.toPath()); 
-
-		return null;
 	}
 }
